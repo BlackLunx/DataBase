6adfaf525a5d6597f88733daa8796dffe8eedf46
Mikhail Zhelein <mikhail.zhelezin@yandex.ru>
2020-02-29 18:05:35 +0300

    added new form and 3 buttons

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 import GUI.Alert;
+import MainLogic.ButtonsMainMenu;
 import MainLogic.Helpers.ReadColumns;
 
 
 import javax.print.DocFlavor;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        ButtonsMainMenu check = new ButtonsMainMenu();
+        check.deleteDB("lol");
         System.exit(0);
     }
 }
--- src/MainLogic/ButtonsMainMenu.java
+++ src/MainLogic/ButtonsMainMenu.java
@@ -1,28 +1,52 @@
 package MainLogic;
 
 import GUI.Alert;
+import MainLogic.Helpers.ReadColumns;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.StringJoiner;
 
 public class ButtonsMainMenu {
+    private String path = "res/bases/";
+    private String format = ".csv";
     public ButtonsMainMenu() {
 
     }
-
+    private void generateFile(String name) throws IOException {
+        ReadColumns reader = new ReadColumns();
+        StringJoiner sj = new StringJoiner(";");
+        for(String column: reader.columns) {
+            sj.add(column);
+        }
+        FileWriter writer = new FileWriter(path + name + format);
+        writer.write(sj.toString());
+        writer.close();
+    }
     public void createDB(String name) throws IOException {
-        File file = new File("res/" +name + ".csv");
-        if(file.createNewFile()) {
+        File file = new File(path + name + format);
+        if(!file.exists()) {
+            generateFile(name);
             generateAlert("Файл успешно создан");
+            return;
         }
         generateAlert("Файл с таким именем уже существует");
     }
-
-    public void openDB(String name) {
-        File file = new File("res/");
-        if(file.exists()) {
-            generateAlert("Такой файл уже существует!");
+    public void deleteDB(String name) {
+        File file = new File(path + name + format);
+        if(file.delete()) {
+            generateAlert("Файл успешно удален");
+            return;
         }
+        generateAlert("Файл с таким именем уже существует");
+    }
+    public void openDB(String name) {
+        File file = new File(path + name + format);
+        if(!file.exists()) {
+            generateAlert("Такого файла не существует");
+        }
+
     }
     private void generateAlert(String message) {
         Alert alert = new Alert(message);
